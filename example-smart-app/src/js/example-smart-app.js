3aa4431a81f8d91b3ab94f034f2790307f5290ec
Suresh Aswathnarayana <suresh.aswathnarayana@live.com>
2019-10-17 20:42:57 -0700

    Update example-smart-app.js
    Testing with height by changing to height1

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -16,7 +16,7 @@
                     query: {
                       code: {
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|29463-7', 'http://loinc.org|29463-7',
+                              'http://loinc.org|29463-7',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
                               'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
                       }
@@ -37,8 +37,8 @@
             lname = patient.name[0].family.join(' ');
           }
 
+          var height1 = byCodes('8302-2');
           var height = byCodes('8302-2');
-          var weight = byCodes('29463-7');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
@@ -49,8 +49,8 @@
           p.gender = gender;
           p.fname = fname;
           p.lname = lname;
-          p.height = getQuantityValueAndUnit(height[0]);
-          p.weight = getQuantityValueAndUnit(weight[0]);
+          p.height = getQuantityValueAndUnit(height1[0]);
+          p.weight = getQuantityValueAndUnit(height[0]);
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
           }
